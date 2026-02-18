e36cd0da212b4831551190ccd426f524b87ff440
Able <volzhang@qq.com>
2026-02-18 20:56:29 +0800

    fix
    优化了初始tiles，方便引导用户学习使用本插件。
    上传了一个更大的初始存档，后续作为默认初始存档。

--- src/vol_apps/tile/tile_store.ts
+++ src/vol_apps/tile/tile_store.ts
@@ -68,12 +68,51 @@ const defaultTile = {
 	id: 0, url: "", meta: {name: "", alt: "", icon: defaultIconBase64, tags: [],}
 };
 
+const TutorialsTiles: Tile[] = [
+	{
+		id: 0, url: "",
+		meta: {
+			name: "Long Press to Drag",
+			alt: "Tutorial: Long press and drag to rearrange tiles",
+			icon: defaultIconBase64,
+			tags: ["tutorial", "step1"],
+		},
+	},
+	{
+		id: 1, url: "",
+		meta: {
+			name: "Right-Click Me",
+			alt: "Tutorial: Right-click (desktop) to edit tile",
+			icon: defaultIconBase64,
+			tags: ["tutorial", "step2"],
+		},
+	},
+	{
+		id: 2, url: "",
+		meta: {
+			name: "Right-Click Beside",
+			alt: "Tutorial: Click beside the tile to open the context menu",
+			icon: defaultIconBase64,
+			tags: ["tutorial", "step3"],
+		},
+	},
+	{
+		id: 3, url: "https://github.com/volzhang/HomePage",
+		meta: {
+			name: "Click Me to Open",
+			alt: "Tutorial: Click the tile to open link in a new tab",
+			icon: defaultIconBase64,
+			tags: ["tutorial", "step4"],
+		},
+	},
+];
+
 type TileStore = TileStoreState & TileStoreActions;
 
 export const useTileStore = create<TileStore>()(
 	persist(
 		(set, get) => ({
-			tiles: [defaultTile],
+			tiles: TutorialsTiles,
 			tileUiVisible: false,
 			tileInEditId: 0,
 			updateTile: (id, updates) => set((state) => {
